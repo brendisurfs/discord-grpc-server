f66b53c6566691fd78f455a24e642f10d08e7f3c
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-09-16 16:37:10 -0700

    udpated build and gitignore
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -1,4 +1,15 @@
+use std::{env, path::PathBuf};
+
 fn main() {
-	tonic_build::compile_protos("./proto/msg.proto")
-		.expect("could not compile proto files");
+	let proto_file = "./proto/msg.proto";
+	let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR not set"));
+
+	tonic_build::configure()
+		.build_server(true)
+		.file_descriptor_set_path(out_dir.join("greeter_descriptor.bin"))
+		.out_dir("./src")
+		.compile(&[proto_file], &["."])
+		.unwrap_or_else(|err| panic!("protobuf compile error: {err}"));
+
+	println!("cargo:rerun-if-changed={}", proto_file);
 }
